0332ea85fac6b53789ee3fd5104fb5cc91af1604
Diego-Urdaneta <91572741+Diego-Urdaneta@users.noreply.github.com>
2022-01-01 20:36:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from Create_tables import check_creation
 from Query import sql_to_df
+import kaggle
 
+#To download files
+kaggle_filepath = input('Input filepath of kaggle files')
+kaggle.api.authenticate()
+kaggle.api.dataset_download_files('stefanoleone992/mutual-funds-and-etfs', path=f'{kaggle_filepath}, unzip=True)
+
+#Information for accessing database                              
 password = input('Enter password for database connection')
 database = input('Enter SQL database name')
 
